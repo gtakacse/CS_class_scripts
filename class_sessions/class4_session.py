241f136e2ed9259c5437c22ba8e1d7059e722353
gtakacse <edit.takacs@gmail.com>
2016-11-12 10:41:05 +0100

    scripts of class 4 - defining functions

---
+++ class_sessions/class4_session.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 19 20:53:38 2016
+
+@author: ASUS
+"""
+
+# CLASS SESSION No.4
+
+# Defining Functions
+def greet1(name):
+        '''….'''
+        print(‘hello’, name)
+ 
+x = greet1(‘Edit’)
+
+def greet2(name):
+        '''….'''
+        return ‘hello ’ + name 
+ 
+y = greet2(‘Edit’)
+
+#Hogyan lehetne a print() és return parancsokat úgy kombinálni, hogy a funkció értéket is visszaadjon és a kiszámított értéket ki is nyomtassa a felhasználónak?
+
+def greet3:
+    pass
+    
+#Írj egy olyan funkciót, amely visszaadja a háromszög területét!
+
+
