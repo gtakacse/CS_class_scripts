06400ffb42d5e268f99e7365498e785b544feec9
gtakacse <edittakacs@Edits-MacBook-Pro.local>
2016-11-15 11:16:29 +0100

    update of the class4 script

--- class_sessions/class4_session.py
+++ class_sessions/class4_session.py
@@ -2,29 +2,61 @@
 """
 Created on Wed Oct 19 20:53:38 2016
 
-@author: ASUS
+@author: GTAKACSE
 """
 
 # CLASS SESSION No.4
 
 # Defining Functions
 def greet1(name):
-        '''….'''
-        print(‘hello’, name)
+    '''
+    Prints out a greeting with name.
+    '''
+    print('hello', name)
  
-x = greet1(‘Edit’)
+x = greet1('Edit')
 
 def greet2(name):
-        '''….'''
-        return ‘hello ’ + name 
+    '''
+    Returns a greeting with name.
+    '''
+    return 'hello ' + name
  
-y = greet2(‘Edit’)
+y = greet2('Edit')
 
 #Hogyan lehetne a print() és return parancsokat úgy kombinálni, hogy a funkció értéket is visszaadjon és a kiszámított értéket ki is nyomtassa a felhasználónak?
 
-def greet3:
-    pass
+# buggy version, mert számít a sorrend: a Python interpreter a return statement utáni sorokat nem hajtja végre
+def greet3(name):
+    '''
+    (str) -> str
+    Print and return greeting with name.'''
+    greet = 'hello' + name
+    return greet
+    print(greet)
+
+gr = greet3('Maria')
+
+# version no.2
+def greet3b(name):
+    '''
+    (str) -> None
+    Print and return greeting with name.'''
+    greet = 'hello' + name
+    print(greet)
+    return greet
+
+# A funkció nyomtat és értéket is visszaad.
+gr2 = greet3b('Emese')
     
 #Írj egy olyan funkciót, amely visszaadja a háromszög területét!
 
+def haromszogT(height, side):
+    '''
+    (number, number) -> flaot
+    Returns the area of a triangle with given side and height.
+    Visszaadja a side oldalú és height magasságú háromszög területét'''
+    return (height * side) / 2
 
+
+t = haromszogT(2,5)
