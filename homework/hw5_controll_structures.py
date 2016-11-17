74fe9775e07eb6538f8092f411abb4ccdfb708c1
gtakacse <edittakacs@Edits-MacBook-Pro.local>
2016-11-17 21:40:48 +0100

    homework 5

---
+++ homework/hw5_controll_structures.py
@@ -0,0 +1,62 @@
+# ==========================================
+#  OTTHONI FELADAT - CONTROL STRUCTURES, BASIC LIST OPERATIONS
+# ==========================================
+# A megoldásokkal kitöltött Python fájlt mentsd el úgy, hogy a fájl neve 'hw5_vezetéknév_keresztnév.py' legyen.
+# A fájlokat az email címemre küldjétek.
+# Határidő: november 21. (hétfő) éjfél
+
+# 1. POZITÍV
+# Írj egy olyan funkciót, amely eldönti egy számról, hogy pozitív vagy sem.
+# A funkció neve legyen is_positive. (A nulla se nem pozitív, se nem negatív.) A funkció adjon vissza igaz/hamis
+# értéket.
+
+# Ide írd a kódodat!
+
+# 2. JELSZÓ
+# Gyakran fordul elő, hogy számítógépes programok használatához felhasználófiókot kell létrehoznunk, amihez jelszóval
+#  tudunk hozzáférni. Ebben a feladatban a jelszókérés és tárolás egy egyszerű formáját fogjuk modellálni.
+# Hozz létre egy funkciót, amely pseudo jelszót kér a felhasználótól (használd hozzá az input funkciót), és a kapott
+# karaktersort visszadja, hogy a funkción kívül is hozzáférhető legyen a felhasználó által megadott jelszó.
+# A funkció azonban a biztonsági kockázat nélkül nem fogadhat el a 6 karakternél rövidebb jelszót, illetve a
+# leggyakoribb jelszó a 'password' sem elfogadható. Ezekben az esetekben a program nem ad vissza semmilyen értéket,
+# csupán azt nyomtatja ki, hogy 'A jelszó nem megfelelő!'
+# A funkció neve legyen ask_pseudo_password!
+
+# Ide írd a kódodat!
+
+# 3. SZÖKŐÉV
+# Írj egy olyan funkciót, amely kiszámítja, hogy a funkciónak megadott évszám szökőév-e vagy sem.
+# Ahhoz, hogy egy év szökőév legyen, oszthatónak kell lennie 4-gyel. Azonban vigyázz a századoknál (tehát ha egy évszám
+# 00-ra végződik) csak akkor szökőév egy év, ha maradék nélkül osztható 400-zal.
+# A funkció attól függően, hogy a megadott évszám 2016 előtti, 2016 utáni, vagy esetleg 2016, a következő üzeneteket
+# írhatja ki a felhasználónak:
+# 2016 szökőév volt. xxxx nem volt szökőév. xxxx szökőév lesz. xxxx nem lesz szökőév.
+# A funkció neve legyen is_leap_year legyen.
+
+# Ide írd a kódodat!
+
+# 4. PYTHON LISTA
+
+list1 = [19,1,7,3,4]
+list2 = ['Emma', 'Anna', 'Viola']
+
+# A fenti lista változók segítésével írj olyan érvényes Python kifejezéseket, amelyek a következő kérdésekre adják
+# meg a választ.
+# 1) Mi list1 harmadik eleme?
+
+# Ide írd a kódodat!
+
+# 2) Igaz-e: list1 hosszabb mint list2?
+
+# Ide írd a kódodat!
+
+# 3) A max() funkcióval állpítsd meg melyik a list1 lista legnagyobb eleme?
+
+# Ide írd a kódodat!
+
+# 4) Hozz létre egy új listát, amelynek tartalma legyen list1 első három elemének és list2 utolsó elemének az együttese!
+# list1 4. eleme legyen a list2 változó értékével azonos!
+
+# Ide írd a kódodat!
+
+
