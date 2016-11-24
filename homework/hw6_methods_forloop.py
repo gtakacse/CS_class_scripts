25e547a76ef28d43856a448f4e7983d01fa66bed
gtakacse <edittakacs@Edits-MacBook-Pro.local>
2016-11-24 13:24:33 +0100

    homework 6

---
+++ homework/hw6_methods_forloop.py
@@ -0,0 +1,62 @@
+# ==========================================
+#  OTTHONI FELADAT - STRING & LIST METHODS, FOR LOOPS
+# ==========================================
+# A megoldásokkal kitöltött Python fájlt mentsd el úgy, hogy a fájl neve 'hw6_vezetéknév_keresztnév.py' legyen.
+# A fájlokat az email címemre küldjétek november 28. éjfélig.
+
+# SZÖVEG MANIPULÁCIÓJA
+# ====================
+# Az s változó értéke Jókai egyik regényének bevezetőjében található. A következőkben string methodok segítségével ezt a részletet kell az instrukciók alapján átalakítanotok.
+s = 'Nem nekünk való az. Mi gazdagok vagyunk és főrangúak. Atyám zászlósúr, s a magyarországi három leggazdagabb birtokosok egyike. Az ő egyetlen leányának más pályát nyitott a sors vágyai kielégítésére. Ott a high life tündérvilága, az udvar Olympja. Ez a mi versenypiacunk.'
+
+# 1)
+# Az s változót alkítsd át kisbetűsre, az eredményt pedig mentsd el az s2 változóban.
+
+# 2)
+# A split method segítségével bontsd az s2 változóban szereplő szöveget mondatokra. Az eredményt mentsd el a sentences változóban.
+
+# 3)
+# A split miatt marad egy üres string a sentences lista végén. Ezt vedd ki, úgy hogy végigmész for ciklussal a
+# sentences változó minden elemén és ha valamelyik elem értéke egyenlő az üres stringgel (i.e. ''), akkor azt az
+# elemet hozzáfűzöd a sentences2 listához, amit lent üres listaként inicializáltam.
+sentences2 = []
+
+# 4)
+# Az sentences2 változó mondataiból (a sentences egy stringeket tartalmazó lista kell legyen; ez a 2) feladatban kért split methodból következik) vedd ki a pontokat, vagyis cseréld ki a pontokat semmire (i.e. ''-re).
+# Hint: for ciklussal tudsz végigmenni a lista mondatain. 
+# A módosított mondatokat pedig fűzzd hozzá sents_without_period üres listához, amit lent találsz.
+sents_without_period = []
+
+# 5)
+# Bonstd a sents_without_period változó mondatait szavakra, és az eredményt mentsd el a words_sents változóba. A words_sents tehát egy lista, amely mondatoknak megfelelő listákat tartalmaz, amelyekben az egyes szavak vannak.
+# Tehát a következő absztrakt feladatot kell végrehajtani:
+# ['első mondat', 'második modat'] -> [['első', 'mondat], ['második', 'mondat]]
+# Hint: minden a listában szereplő mondaton for ciklussal tudsz végigmenni, a split method segítségével pedig szavakra tudok őket szedni.
+
+# 6)
+# Ha megnézed a words_sents változó értékét, láthatod, hogy a vesszők a szóalak részeként kerültek bele. Javítsuk ezt
+#  úgy ki, hogy a words_sents2 új változóhoz hozzáadjuk a vessző szempontjából már kijavított mondatokat.
+# Hint: Mivel a replace method csak stringeken és nem listákon működik, ezért két for ciklust kell használni,
+# amiből az egyik végigmegy az összes mondaton, a másik pedig a mondat összes szaván. Ennek a következő a templateje:
+
+l1 = [[1,2], [3,4]] #lista, ami listákat tartalmaz
+l2 = [] # új lista inicializálása, ami kérsőbb majd a módosított elemeket fogja tartalmazni
+for sublist in l1:
+    new_sublist = [] # a belső listákat helyettesítő új lista inicializálása
+    for item in sublist:
+        new_sublist.append(item+1) # az allistához hozzáadom a módosított elemet, jelen esetben hozzáadok 1-et
+    l2.append(new_sublist) # az új listához hozzáadom az új allistát, miután minden módosított elemet hozzáadtam
+print(l2) # a folyamat végén l2 értéke: [[2, 3], [4, 5]]
+
+# Ha nem vagy biztos abban, hogy az egyes sorban mit csinál a gép, a kódvizalizációs eszköz segíteni tud (http://www.pythontutor.com/visualize.html#mode=edit).
+
+words_sents2 = []
+
+# 7)
+# Csinálj egy egyszerű szótárt az s2 váltzóból (egy listát, amely a szövegben előforduló szavakat tartalmazza egyettlen egyszer egyszer). A szólistát mentsd el a vocab változóban, amelyet lent üres listaként inicializáltam neked. A
+# szóalakokat úgy add hozzá a vocab változóhoz, hogy nem tartalmaz vesszőt vagy pontot.
+# Hint: Az in (tagadása a not in) operátor segítségével tudod megnézni, hogy egy elem része-e egy listának. Tehát ha a szöveg egy szava nem része a vocab listának, akkor és csakis akkor kell hozzáadni a vocab listához.
+vocab = []
+
+
+
---
+++ homework/test_hw6.py
@@ -0,0 +1,163 @@
+import os
+import re
+import importlib
+
+def main():
+    s2_sol = 'nem nekünk való az. mi gazdagok vagyunk és főrangúak. atyám zászlósúr, s a magyarországi három leggazdagabb birtokosok egyike. az ő egyetlen leányának más pályát nyitott a sors vágyai kielégítésére. ott a high life tündérvilága, az udvar olympja. ez a mi versenypiacunk.'
+
+    sentences_sol = ['nem nekünk való az',
+                     ' mi gazdagok vagyunk és főrangúak',
+                     ' atyám zászlósúr, s a magyarországi három leggazdagabb birtokosok egyike',
+                     ' az ő egyetlen leányának más pályát nyitott a sors vágyai kielégítésére',
+                     ' ott a high life tündérvilága, az udvar olympja',
+                     ' ez a mi versenypiacunk',
+                     '']
+
+    sentences2_sol = ['nem nekünk való az',
+                      ' mi gazdagok vagyunk és főrangúak',
+                      ' atyám zászlósúr, s a magyarországi három leggazdagabb birtokosok egyike',
+                      ' az ő egyetlen leányának más pályát nyitott a sors vágyai kielégítésére',
+                      ' ott a high life tündérvilága, az udvar olympja',
+                      ' ez a mi versenypiacunk']
+
+    sents_without_period_sol = ['nem nekünk való az',
+                                ' mi gazdagok vagyunk és főrangúak',
+                                ' atyám zászlósúr, s a magyarországi három leggazdagabb birtokosok egyike',
+                                ' az ő egyetlen leányának más pályát nyitott a sors vágyai kielégítésére',
+                                ' ott a high life tündérvilága, az udvar olympja',
+                                ' ez a mi versenypiacunk']
+
+    words_sents_sol = [['nem', 'nekünk', 'való', 'az'],
+                       ['mi', 'gazdagok', 'vagyunk', 'és', 'főrangúak'],
+                       ['atyám',
+                        'zászlósúr,',
+                        's',
+                        'a',
+                        'magyarországi',
+                        'három',
+                        'leggazdagabb',
+                        'birtokosok',
+                        'egyike'],
+                       ['az',
+                        'ő',
+                        'egyetlen',
+                        'leányának',
+                        'más',
+                        'pályát',
+                        'nyitott',
+                        'a',
+                        'sors',
+                        'vágyai',
+                        'kielégítésére'],
+                       ['ott', 'a', 'high', 'life', 'tündérvilága,', 'az', 'udvar', 'olympja'],
+                       ['ez', 'a', 'mi', 'versenypiacunk']]
+
+    words_sents2_sol = [['nem', 'nekünk', 'való', 'az'],
+                        ['mi', 'gazdagok', 'vagyunk', 'és', 'főrangúak'],
+                        ['atyám',
+                         'zászlósúr',
+                         's',
+                         'a',
+                         'magyarországi',
+                         'három',
+                         'leggazdagabb',
+                         'birtokosok',
+                         'egyike'],
+                        ['az',
+                         'ő',
+                         'egyetlen',
+                         'leányának',
+                         'más',
+                         'pályát',
+                         'nyitott',
+                         'a',
+                         'sors',
+                         'vágyai',
+                         'kielégítésére'],
+                        ['ott', 'a', 'high', 'life', 'tündérvilága', 'az', 'udvar', 'olympja'],
+                        ['ez', 'a', 'mi', 'versenypiacunk']]
+
+    vocab_sol = ['nem',
+                 'nekünk',
+                 'való',
+                 'az',
+                 'mi',
+                 'gazdagok',
+                 'vagyunk',
+                 'és',
+                 'főrangúak',
+                 'atyám',
+                 'zászlósúr',
+                 's',
+                 'a',
+                 'magyarországi',
+                 'három',
+                 'leggazdagabb',
+                 'birtokosok',
+                 'egyike',
+                 'ő',
+                 'egyetlen',
+                 'leányának',
+                 'más',
+                 'pályát',
+                 'nyitott',
+                 'sors',
+                 'vágyai',
+                 'kielégítésére',
+                 'ott',
+                 'high',
+                 'life',
+                 'tündérvilága',
+                 'udvar',
+                 'olympja',
+                 'ez',
+                 'versenypiacunk']
+
+    file = find_file()
+    modul = load_modul(file)
+    scores = 0
+    total_score=7
+    scores += test_variables(modul.s2, s2_sol, 's2')
+    scores += test_variables(modul.sentences, sentences_sol, 'sentences')
+    scores += test_variables(modul.sentences2, sentences2_sol, 'sentences2')
+    scores += test_variables(modul.sents_without_period, sents_without_period_sol, 'sens_without_period')
+    scores += test_variables(modul.words_sents, words_sents_sol, 'word_sents')
+    scores += test_variables(modul.words_sents2, words_sents2_sol, 'words_sents2')
+    scores += test_variables(modul.vocab, vocab_sol, 'vocab')
+    get_stats(scores, total_score)
+
+def find_file():
+    files = os.listdir(os.curdir)
+    for file in files:
+        if re.search(r'^hw6.*py$', file) is not None and file != 'hw6_methods_forloop.py':
+            return file
+
+def load_modul(file):
+    try:
+        modul = importlib.import_module(file[:-3])
+        print('Trying to load the homework file with the name hw6_name.py\n' + '='*56)
+        return modul
+    except ImportError:
+        print('The {} function cannot be found in {}'.format(file))
+
+def test_variables(stud_solution, expected, variable_name):
+    print('For variable {}'.format(variable_name))
+    print('Expected: {}'.format(expected))
+    print('Received: {}'.format(stud_solution))
+    if stud_solution == expected:
+        print('The two match. Great job.')
+        print('=' * 56)
+        return 1
+    else:
+        print('The two DO NOT match. Keep trying.')
+        print('=' * 56)
+        return 0
+
+def get_stats(scores, total):
+    print('Summary:\n{} out of {}: {}%'.format(scores, total, round(scores / total * 100), 2))
+    print('=' * 56)
+    print('*' * 56)
+    print('=' * 56)
+
+if __name__ == '__main__':
+    main()
