749032e8b69ad5c48296772d8ca4187d118782f3
gtakacse <edittakacs@Edits-MacBook-Pro.local>
2016-12-09 13:14:34 +0100

    homework 8

---
+++ homework/hw8_file_operations.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# ==========================================
+#  OTTHONI FELADAT - FÁJLOK TARTALMÁNAK BEOLVASÁSA, FÁJLOK ÍRÁSA
+# ==========================================
+# A megoldásokkal kitöltött Python fájlt mentsd el úgy, hogy a fájl neve 'hw8_vezetéknév_keresztnév.py' legyen.
+# A fájlokat (hw_név.py és az eredmény txt fájlját) az email címemre küldjétek el december 13. (kedd) éjfélig.
+
+# 1)
+# Olvasd be az egy_az_isten.txt fájl tartalmát Pythonba, ami Jókai Mór Egy az isten című regényét tartalmazza. (A regényt a Magyar Elektronikus Könyvtárból töltöttem le.)
+
+# 2)
+# Egy külön változóba gyűjtsd ki a 'd' betűvel kezdődő szavakat. Ne feledkezz meg a nagybetűs szavakról sem. (Érdemes listát használni a változóhoz.)
+
+# 3) 
+# A 'd' betűs szavakat tartalmazó szavakat írd ki egy fájlba úgy, hogy minden szó külön sorba kerüljön. A fájl legyen txt formátumú, de a neve szabadon választható.
